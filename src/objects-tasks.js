123c842ca0313dbaa38aa81aa59bc2c9dae0b92a
Pavel Rabekin <pavelrabekin@gmail.com>
2024-03-11 23:39:00 +0300

    feat: task 'removeProperties' is done

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -59,8 +59,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  let newObj = {};
+  newObj = Object.assign(newObj, obj);
+  keys.forEach((key) => {
+    delete newObj[key];
+  });
+  return newObj;
 }
 
 /**
